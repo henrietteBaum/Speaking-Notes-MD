5e443c45791e6cd2562df815a60d4d310111128a
henrietteBaum <henriette-baum@posteo.de>
2022-03-30 14:08:00 +0200

    update Help

--- speaking-notes-v.4.1.0/src/components/Help.jsx
+++ speaking-notes-v.4.1.0/src/components/Help.jsx
@@ -9,33 +9,34 @@ const Help = () => {
     </div>
 
     <hr/>
-    <p>
+    <p align='center'>
       Markdown Reference: &nbsp;  
       <a href='https://www.markdownguide.org/basic-syntax/' >Marcdownguide.org</a>   
     </p>
-
+    <hr/>
+    <br/>
     <h2>Headings:</h2>
       <p>
         # H1, ## H2, ### H3
       </p>
     <h2>Bold text:</h2>
-      <p>
-        **some text**
-      </p>
+        <p>**some text**</p>
      <h2>Blockquote:</h2>
-     <p>&gt;your quote</p>
+        <p>&gt;your quote</p>
      <h2>Unordered list:</h2>
-      <p>-item</p>
-      <p>-item</p> 
-      <p>-item</p>
+        <p>-item</p>
+        <p>-item</p> 
+        <p>-item</p>
      <h2>Ordered list:</h2>
-      <p>1. item one</p>
-      <p>2. item one</p>
-      <p>3. item one</p>
+        <p>1. item one</p>
+        <p>2. item one</p>
+        <p>3. item one</p>
      <h2>Code:</h2>
-      <p>
-        `some code`
-      </p>    
+        <p>`some code`</p>    
+     <h2>Link:</h2>
+        <p>[title](https://www.example.com)</p>
+     <h2>Image:</h2>
+        <p>![alt text](image.jpg)</p>
     </StyledHelp>
   )
 }
