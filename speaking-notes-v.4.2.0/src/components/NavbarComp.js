2dc45afa8cb46076a3efcdf7734c5ff89de30126
henrietteBaum <henriette-baum@posteo.de>
2022-04-04 22:18:55 +0200

    add Stop-button

--- speaking-notes-v.4.2.0/src/components/NavbarComp.js
+++ speaking-notes-v.4.2.0/src/components/NavbarComp.js
@@ -1,13 +1,12 @@
 import logo from '../react-logo512-red.png';
-import React, { useRef, useContext} from 'react';
-import {Container, Nav, Navbar, NavDropdown, Form} from 'react-bootstrap';
-import {Link} from 'react-router-dom';
+import React, { useRef, useContext } from 'react';
+import { Container, Nav, Navbar, NavDropdown, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { NoteContext } from './NoteContext';
 
-function NavbarComp(){
+function NavbarComp() {
 
   const [noteText, setNoteText] = useContext(NoteContext);
-  
 
   // open file:
   let handleFileOpenChange = (e) => {
@@ -24,27 +23,27 @@ function NavbarComp(){
     }
   }
 
-    // save note text to file
+  // save note text to file
 
-    let handleBtnSave = () => {
-      const element = document.createElement('a');
-      const file = new Blob([noteText], {
-        type:'text/plain; charset=utf-8'
-      })
-      element.href = URL.createObjectURL(file);
-      element.download = 'untitled.md'
-      // element.download = filenameSave ? `${filenameSave}` : 'untitled.md';
-      document.body.appendChild(element);
-      element.click();
-    }
-  
+  let handleBtnSave = () => {
+    const element = document.createElement('a');
+    const file = new Blob([noteText], {
+      type: 'text/plain; charset=utf-8'
+    })
+    element.href = URL.createObjectURL(file);
+    element.download = 'untitled.md'
+    // element.download = filenameSave ? `${filenameSave}` : 'untitled.md';
+    document.body.appendChild(element);
+    element.click();
+  }
 
-    // textarea
-    const handleBtnClear = () => {
-      setNoteText('');
-    }
   
-  
+  // textarea
+  const handleBtnClear = () => {
+    setNoteText('');
+  }
+
+
   // Speechsynthesis
   const syntRef = useRef(window.speechSynthesis);
 
@@ -54,33 +53,37 @@ function NavbarComp(){
     console.log(utterance);
   }
 
+  const handleBtnStop = () => {
+    syntRef.current.cancel();
+  }
 
   return (
     <Navbar bg="dark" variant="dark" expand="sm">
       <Container fluid>
-        <Navbar.Brand as={Link} to="/" style={{color:'orange'}}>
+        <Navbar.Brand as={Link} to="/" style={{ color: 'orange' }}>
           <img src={logo} width='36px' alt='logo' /> Speaking Markdown
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto" >
             <NavDropdown title="File" id="basic-nav-dropdown" className='linkText' menuVariant='dark' bg="dark">
-            <Form.Group controlId="formFileMb" className="mb-3">
-              <Form.Label visuallyHidden='true' >Open exiisting file</Form.Label>
-              <Form.Control onChange={handleFileOpenChange} type="file" className='form-control form-control-mb-3 bg-dark text-white' />
-            </Form.Group>
+              <Form.Group controlId="formFileMb" className="mb-3">
+                <Form.Label visuallyHidden='true' >Open exiisting file</Form.Label>
+                <Form.Control onChange={handleFileOpenChange} type="file" className='form-control form-control-mb-3 bg-dark text-white' />
+              </Form.Group>
               <NavDropdown.Item onClick={handleBtnSave} href="#action/3.2">Save</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item onClick={handleBtnSpeak} href="#action/3.3">Speak</NavDropdown.Item>
+              <NavDropdown.Item onClick={handleBtnStop} href="#action/4.3">Stop</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item onClick={handleBtnClear} href="#action/4.4">Clear</NavDropdown.Item>
             </NavDropdown>
-                <Nav.Link as={Link} to="/" className='linkText'>Editor</Nav.Link> 
-                <Nav.Link as={Link} to="/preview" className='linkText'>Preview</Nav.Link> 
-                <Nav.Link as={Link} to="/help" className='linkText'>Help</Nav.Link> 
-           </Nav>
-          </Navbar.Collapse>
-        </Container>
+            <Nav.Link as={Link} to="/" className='linkText'>Editor</Nav.Link>
+            <Nav.Link as={Link} to="/preview" className='linkText'>Preview</Nav.Link>
+            <Nav.Link as={Link} to="/help" className='linkText'>Help</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
